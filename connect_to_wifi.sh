4791900347eb915011182e201a00fc366ef62f58
nsatyarthy <6873481+nsatyarthy@users.noreply.github.com>
2019-01-25 09:19:50 +0900

    kill wpa_supplicant before trying to bring it up

--- connect_to_wifi.sh
+++ connect_to_wifi.sh
@@ -4,6 +4,7 @@ echo "Will now try to connect to WiFi"
 
 modeprobe -rf mwifiex_pcie
 modeprobe -if mwifiex_pcie
+killall wpa_supplicant
 wpa_supplicant -B -c/etc/wpa_supplicant/wpa_supplicant.conf -iwlp1s0
 dhcpcd wlp1s0
 ping -c 4 www.google.com
